12da9113d73cd307ae99febc8fab8101ebeccee8
Abdulghani Jalal <89863968+Jalal-Abdulghani@users.noreply.github.com>
2023-03-16 17:27:37 -0400

    Abouds first commit

--- src/parkingBookingSystem/Client.java
+++ src/parkingBookingSystem/Client.java
@@ -5,6 +5,7 @@ public class Client {
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD");
 		System.out.println("HELLO FROM ANMOl");
+		System.out.println("HELLO FROM ABOUDI");
 
 	}
 
