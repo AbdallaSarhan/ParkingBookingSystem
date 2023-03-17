666a2195849179071f4ce96a873acb3c8458e7e5
AbdallaSarhan <abdalla1@my.yorku.ca>
2023-03-17 18:55:37 -0400

    faculty member class modified

--- src/userModule/FacultyMember.java
+++ src/userModule/FacultyMember.java
@@ -4,6 +4,8 @@ public class FacultyMember extends Client {
 	
 	private Management managmentTeam;
 	
+	public final int RATE = 8;
+	
 	public FacultyMember(String name, String email, String password, String licensePlate) {
 		super(name, email, password, licensePlate);
 	}
