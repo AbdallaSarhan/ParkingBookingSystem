7843be42b24546577b6cca6fff30cbd1d22226b0
AbdallaSarhan <abdalla1@my.yorku.ca>
2023-03-16 21:02:17 -0400

    Booking and Booking system set up

---
+++ src/parkingBookingModule/Booking.java
@@ -0,0 +1,48 @@
+package parkingBookingModule;
+
+import java.time.LocalDateTime;
+
+public class Booking {
+	
+	private int id;
+	private LocalDateTime startDate;
+	private LocalDateTime endDate;
+	private ParkingSpace parkingSpace;
+	private String licensePlate;
+	
+	
+	public Booking(LocalDateTime startDate, LocalDateTime endDate, ParkingSpace parkingSpace, String licensePlate) {
+	
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.parkingSpace = parkingSpace;
+		this.licensePlate = licensePlate;
+		
+	}
+
+
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+
+
+	public ParkingSpace getParkingSpace() {
+		return parkingSpace;
+	}
+
+
+	public LocalDateTime getEndDate() {
+		return endDate;
+	}
+
+
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+}
--- src/parkingBookingModule/BookingSystem.java
+++ src/parkingBookingModule/BookingSystem.java
@@ -3,10 +3,50 @@ package parkingBookingModule;
 import java.util.ArrayList;
 
 import paymentModule.PaymentMethod;
+import userModule.Client;
 
 public class BookingSystem {
 	private ArrayList<ParkingSpace> parkingSpaces;
 	private ArrayList<ParkingLot> parkinglots;
 	private PaymentMethod payment;
+	
+	public ArrayList<ParkingSpace> getAvailableSpaces(){
+		
+		return null;
+	}
+	
+	public Booking bookParkingSpace(Client user, ParkingSpace parkingSpace) {
+	
+		
+		return null;
+		
+	}
+	
+	public boolean payForBooking(Client user, Booking bookingInfo) {
+		
+		return false;
+	}
+	
+	public Booking editBooking(Client user, ParkingSpace parkingSpace) {
+		
+		
+		return null;
+		
+	}
+	
+	public Booking cancelBooking(Client user, ParkingSpace parkingSpace) {
+		
+		return null;
+	}
+	
+	public Booking extendParkingDuration(Client user, ParkingSpace parkingSpace) {
+		
+		return null;
+	}
+	
+	public Booking getCarInfo() {
+		
+		return null;
+	}
 
 }
