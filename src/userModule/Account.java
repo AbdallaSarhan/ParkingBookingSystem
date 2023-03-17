5c3c4d319c84384674914db570cd44f5dae901cb
anmol02singh <anmol_singh02@outlook.com>
2023-03-16 20:26:31 -0400

    added variables

---
+++ src/userModule/Account.java
@@ -0,0 +1,17 @@
+package userModule;
+
+public class Account {
+	private String email;
+	private String password;
+	private String name;
+	
+	public Account (String email, String password, String name) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+	}
+	
+	public void login(String name, String password) {
+		
+	}
+}
